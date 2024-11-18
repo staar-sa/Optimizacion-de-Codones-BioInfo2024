d63a3253a7c80024de5e433b171a6b0683c6420c
Daniela Villanueva <dvillanueva16@alumnos.uaq.mx>
2024-11-18 13:52:59 -0600

    Delete codones_borrador directory

--- codones_borrador/secuencias.R
+++
@@ -1,8 +0,0 @@
-###Secuencias fasta
-##Cargar libreria 
-
-library(Biostrings)
-
-##Leer las secuencias fasta para que luego las pongamos en un data frame 
-
-fasta_ecoli <- "ruta del archivofasta" readAAStringSet()
