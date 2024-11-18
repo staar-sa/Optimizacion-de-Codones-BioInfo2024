dc5a3037940132d19bfdd40a878cc3c98f6d496f
Daniela Villanueva <dvillanueva16@alumnos.uaq.mx>
2024-11-18 14:51:49 -0600

    Update secuencias_fasta.R

--- Scripts/secuencias_fasta.R
+++ Scripts/secuencias_fasta.R
@@ -4,3 +4,15 @@
 ##Cargar libraría siempre
 
 library(Biostrings)
+
+ecoli <- "01_raw_data/e.coli.fna"
+ecoli_fasta <- readAAStringSet(ecoli)
+
+bsubtilis <- "01_raw_data/b. subtilis.fna"
+bsubtilis_fasta <- readAAStringSet (bsubtilis)
+
+pflour <- "01_raw_data/b. subtilis.fna"
+flouresce_fasta <- readAAStringSet (pflour)
+
+cereviseae <- "01_raw_data/s.cereviseae.fna"
+cereviseae_fasta <- readAAStringSet (cereviseae)
