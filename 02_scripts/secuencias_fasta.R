314d60552fa7be597bec9897a8aa4d296b5db47c
Daniela Villanueva <dvillanueva16@alumnos.uaq.mx>
2024-11-18 15:22:14 -0600

    Actualizacion

--- 02_scripts/secuencias_fasta.R
+++ 02_scripts/secuencias_fasta.R
@@ -14,8 +14,6 @@ bsubtilis_fasta <- readAAStringSet (bsubtilis)
 pflour <- "01_raw_data/b. subtilis.fna"
 flouresce_fasta <- readAAStringSet (pflour)
 
-cereviseae <- "01_raw_data/s.cereviseae.fna"
-cereviseae_fasta <- readAAStringSet (cereviseae)
 
 # Crear un data frame con las secuencias
 secuencias_df <- rbind(    ###Es para unir todos los data frames, porque no se como hacerlo o no me acuerdo jejej 
@@ -37,12 +35,8 @@ secuencias_df <- rbind(    ###Es para unir todos los data frames, porque no se c
     Secuencia = as.character(flouresce_fasta),
     Longitud = width(flouresce_fasta)
     
-  ),
-  data.frame(
-    Organismo = "S. cereviseae",
-    ID = names(cereviseae_fasta),
-    Secuencia = as.character(cereviseae_fasta),
-    Longitud = width(cereviseae_fasta)
-    
   )
 )
+
+
+
