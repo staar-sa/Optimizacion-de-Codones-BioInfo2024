b39038f38ef9aed81e527c8f0f8cd6eb1145fa68
Daniela Villanueva <dvillanueva16@alumnos.uaq.mx>
2024-11-18 15:11:47 -0600

    Update secuencias_fasta.R

--- 02_scripts/secuencias_fasta.R
+++ 02_scripts/secuencias_fasta.R
@@ -16,3 +16,33 @@ flouresce_fasta <- readAAStringSet (pflour)
 
 cereviseae <- "01_raw_data/s.cereviseae.fna"
 cereviseae_fasta <- readAAStringSet (cereviseae)
+
+# Crear un data frame con las secuencias
+secuencias_df <- rbind(    ###Es para unir todos los data frames, porque no se como hacerlo o no me acuerdo jejej 
+  data.frame(
+    Organismo = "E. coli",
+    ID = names(ecoli_fasta),
+    Secuencia = as.character(ecoli_fasta),
+    Longitud = width(ecoli_fasta)
+  ),
+  data.frame(
+    Organismo = "B. subtilis",
+    ID = names(bsubtilis_fasta),
+    Secuencia = as.character(bsubtilis_fasta),
+    Longitud = width(bsubtilis_fasta)
+  ),
+  data.frame(
+    Organismo = "P. fluorescens",
+    ID = names(flouresce_fasta),
+    Secuencia = as.character(flouresce_fasta),
+    Longitud = width(flouresce_fasta)
+    
+  ),
+  data.frame(
+    Organismo = "S. cereviseae",
+    ID = names(cereviseae_fasta),
+    Secuencia = as.character(cereviseae_fasta),
+    Longitud = width(cereviseae_fasta)
+    
+  )
+)
