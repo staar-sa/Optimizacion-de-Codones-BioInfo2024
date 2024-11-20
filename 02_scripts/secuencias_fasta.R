36f70d1143d09eb451c1134eedef1423037dc228
staar-sa <esegobia23@alumnos.uaq.mx>
2024-11-19 20:18:33 -0600

    actualizacion

--- 02_scripts/secuencias_fasta.R
+++ 02_scripts/secuencias_fasta.R
@@ -72,173 +72,117 @@ print(secuencias_df)
 
 
 ##2 - DEFINIMOS UNA FUNCION PARA DETERMINAR LA PREFERENCIA DE CODONES
-
-
-##############################################################################
-
-
-# Función que determina la preferencia de codones de 
-
-
-##Definir una función, para que esta reciba una secuencia de ADN como entrada
-
+  #Definir una función, para que esta reciba una secuencia de ADN como entrada
 preferencia_de_codones <- function(secuencia) {
 
-# Dividir una secuencia en codones
-# Convertir DNAString a caracteres
+ #2.1 Dividir una secuencia en codones
   
+  # Convertir DNAString a caracteres
 secuencia_en_caracteres <- as.character(secuencia)
-  
-# Obtener la longitud de la secuencia
+  # Obtener la longitud de la secuencia
 numero_caracteres <- nchar( secuencia_en_caracteres)
-  
-# Verificar si la longitud es múltiplo de 3
+  #Verificar si la longitud es múltiplo de 3
 if (numero_caracteres %% 3 != 0) {
 secuencia_en_caracteres <- substr( secuencia_en_caracteres, 1, numero_caracteres
                                    - (numero_caracteres %% 3))
 }
-  
-# Dividir en codones
+  #Dividir en codones
 codones <- substring( secuencia_en_caracteres, 
                        seq(1, nchar( secuencia_en_caracteres)-2, by=3),
                        seq(3, nchar( secuencia_en_caracteres), by=3))
   
-
-
+#2.2 Creamos un dataframe sobre los codones que le corresponden a cada aminoacido
 codones_aminoacidos <- data.frame(
   Codon = c(
     # Alanina
     "GCT", "GCC", "GCA", "GCG",
-    
     # Cisteína
     "TGT", "TGC",
-    
     # Ácido aspártico
     "GAT", "GAC",
-    
     # Ácido glutámico
     "GAA", "GAG",
-    
     # Fenilalanina
     "TTT", "TTC",
-    
     # Glicina
     "GGT", "GGC", "GGA", "GGG",
-    
     # Histidina
     "CAT", "CAC",
-    
     # Isoleucina
     "ATT", "ATC", "ATA",
-    
     # Lisina
     "AAA", "AAG",
-    
     # Leucina
     "TTA", "TTG", "CTT", "CTC", "CTA", "CTG",
-    
     # Metionina
     "ATG",
-    
     # Asparagina
     "AAT", "AAC",
-    
     # Prolina
     "CCT", "CCC", "CCA", "CCG",
-    
     # Glutamina
     "CAA", "CAG",
-    
     # Arginina
     "CGT", "CGC", "CGA", "CGG", "AGA", "AGG",
-    
     # Serina
     "TCT", "TCC", "TCA", "TCG", "AGT", "AGC",
-    
     # Treonina
     "ACT", "ACC", "ACA", "ACG",
-    
     # Valina
     "GTT", "GTC", "GTA", "GTG",
-    
     # Triptófano
     "TGG",
-    
     # Tirosina
     "TAT", "TAC",
-    
-    # Codones de parada
+    # Codones de paro
     "TAA", "TAG", "TGA"
   ),
-  
   Aminoacido = c(
     # 4 Alanina
     "Ala", "Ala", "Ala", "Ala",
-    
     # 2 Cisteína
     "Cys", "Cys",
-    
     # 2 Ácido aspártico
     "Asp", "Asp",
-    
     # 2 Ácido glutámico
     "Glu", "Glu",
-    
     # 2 Fenilalanina
     "Phe", "Phe",
-    
     # 4 Glicina
     "Gly", "Gly", "Gly", "Gly",
-    
     # 2 Histidina
     "His", "His",
-    
     # 3 Isoleucina
     "Ile", "Ile", "Ile",
-    
     # 2 Lisina
     "Lys", "Lys",
-    
     # 6 Leucina
     "Leu", "Leu", "Leu", "Leu", "Leu", "Leu",
-    
     # 1 Metionina
     "Met",
-    
     # 2 Asparagina
     "Asn", "Asn",
-    
     # 4 Prolina
     "Pro", "Pro", "Pro", "Pro",
-    
     # 2 Glutamina
     "Gln", "Gln",
-    
     # 6 Arginina
     "Arg", "Arg", "Arg", "Arg", "Arg", "Arg",
-    
     # 6 Serina
     "Ser", "Ser", "Ser", "Ser", "Ser", "Ser",
-    
     # 4 Treonina
     "Thr", "Thr", "Thr", "Thr",
-    
     # 4 Valina
     "Val", "Val", "Val", "Val",
-    
     # 1 Triptófano
     "Trp",
-    
     # 2 Tirosina
     "Tyr", "Tyr",
-    
     # 3 Codones de parada
     "Stop", "Stop", "Stop"
-  )
-)  
+  ))  
 
-
-# Crear un vector de tamaño igual al número de codones, inicializado en 0
+ #2.3 Crear un vector de tamaño igual al número de codones, inicializado en 0
 frecuencia_codones <- numeric(length(codones_aminoacidos$Codon))
 # Asignar nombres a cada posición del vector, basados en los codones del data frame
 names(frecuencia_codones) <- codones_aminoacidos$Codon
